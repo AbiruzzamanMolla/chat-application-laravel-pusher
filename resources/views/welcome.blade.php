c292c548ef9ddf95dbd4405c7da89ba557f08608
Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2023-03-17 18:18:49 +0600

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="{{ asset('build\assets\app-67dcdfd2.css') }}" rel="stylesheet">
     <link href="{{ asset('js/toster/build/toastr.min.css') }}" rel="stylesheet">
-    @vite(['resources/css/app.css' , 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css' , 'resources/js/app.js']) --}}
     <title>Live Chat</title>
 
 </head>
@@ -22,7 +23,7 @@
                 <div class="col-md-10 text-center">
                     <h3>Chat App</h3>
                 </div>
-                <div class="col-md-2"> <a class="btn btn-danger" href="{{ route('logout') }}"
+                <div class="col-md-2 d-flex justify-content-end"> <a class="btn btn-danger" href="{{ route('logout') }}"
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
                     </a>
@@ -34,20 +35,19 @@
             </div>
         </div>
         <div class="row pt-4">
-            <div class="col-sm-6 offset-sm-3 my-2">
-                <input type="text" class="form-control" placeholder="Enter a user .........."
-                    value="{{ auth()->user()->name }}" disabled>
-                <input type="hidden" class="form-control" name="username" id="username"
-                    value="{{ auth()->user()->name }}">
-            </div>
-
             <div class="col-sm-6 offset-sm-3">
                 <div class="card box-primary direct-chat direct-chat-primary">
-
+                    <div class="card-header">
+                        <h2>{{ auth()->user()->name }}</h2>
+                    </div>
+                    <input type="hidden" class="form-control" name="username" id="username"
+                        value="{{ auth()->user()->name }}">
                     <div class="card-body">
                         <div class="direct-chat-messages" id="messages">
                             @forelse ($messages as $chat)
-                            <p @if($chat->username == auth()->user()->name)class="d-flex justify-content-end"@endif><strong>{{ $chat->username }}: </strong>{{ $chat->message }}</p>
+                            <p @if($chat->username == auth()->user()->name)class="d-flex
+                                justify-content-end"@endif><strong>{{ $chat->username }}: </strong>{{ $chat->message }}
+                            </p>
                             @empty
                             <p id="emptyMessage">No message history!</p>
                             @endforelse
@@ -61,7 +61,7 @@
                                     class="form-control">
                                 <span class="input-group-btn">
                                     <button type="submit" id="send_message"
-                                        class="btn btn-primary btn-flat">Send</button>
+                                        class="btn btn-primary btn-flat" style="color: white; back">Send</button>
                                 </span>
                             </div>
                         </form>
@@ -86,6 +86,7 @@
         });
 
     </script>
+    <script src="{{ asset('build\assets\app-d822b08c.js') }}"></script>
 </body>
 
 </html>
